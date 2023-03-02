8b83c9b83119dcc4fe16cf6f5accfbb3fcb011fc
Abhijeet Ingle <abhijeetingle.p@gmail.com>
2023-03-02 22:22:12 +0530

    added seeders for album and songs

---
+++ backend/db/seeders/20230302161129-demo-songs.js
@@ -0,0 +1,116 @@
+"use strict";
+
+const { album, song } = require("../models");
+
+const albumSongs = [
+  {
+    albumTitle: "Dark",
+    songs: [
+      {
+        title: "Sinister Power Rising",
+      },
+      {
+        title: "Black Box",
+      },
+    ],
+  },
+  {
+    albumTitle: "StarStuck",
+    songs: [
+      {
+        title: "Ambient Dreamy Clouds",
+      },
+    ],
+  },
+  {
+    albumTitle: "Power",
+    songs: [
+      {
+        title: "Energy Pill",
+      },
+    ],
+  },
+  {
+    albumTitle: "Pink",
+    songs: [
+      {
+        title: "Can't Fall In Love",
+      },
+    ],
+  },
+  {
+    albumTitle: "Melody",
+    songs: [
+      {
+        title: "In The Forest",
+      },
+    ],
+  },
+  {
+    albumTitle: "Just do it",
+    songs: [
+      {
+        title: "Legendary Piano",
+      },
+    ],
+  },
+  {
+    albumTitle: "Beats",
+    songs: [
+      {
+        title: "Stuck In A Dream",
+      },
+    ],
+  },
+  {
+    albumTitle: "Remembrance",
+    songs: [
+      {
+        title: "Electric",
+      },
+      {
+        title: "Stomp Rap",
+      },
+    ],
+  },
+];
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    for (let idx = 0; idx < albumSongs.length; idx++) {
+      const { albumTitle, songs } = albumSongs[idx];
+      const albumObj = await album.findOne({ where: { title: albumTitle } });
+      for (let songIdx = 0; songIdx < songs.length; songIdx++) {
+        const songObj = songs[songIdx];
+        await song.create({ ...songObj, album_id: albumObj.id });
+      }
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    for (let idx = 0; idx < albumSongs.length; idx++) {
+      const { albumTitle, songs } = albumSongs[idx];
+      const albumObj = await album.findOne({ where: { title: albumTitle } });
+      for (let songIdx = 0; songIdx < songs.length; songIdx++) {
+        const songObj = songs[songIdx];
+        await song.destroy({ ...songObj, album_id: albumObj.id });
+      }
+    }
+  },
+};
