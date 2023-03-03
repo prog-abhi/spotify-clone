ac3620eb4959ec819a954f70c395abd9dd51be8b
Abhijeet Ingle <abhijeetingle.p@gmail.com>
2023-03-03 17:45:35 +0530

    added some security middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,39 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const csurf = require("csurf");
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
+const isProduction = process.env.NODE_ENV === "production";
 
 const app = express();
 
-app.use(morgan("dev", "tiny"));
+app.use(cookieParser());
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "lax",
+      httpOnly: true,
+    },
+  })
+);
+if (!isProduction) app.use(cors());
 
+app.use(morgan("dev", "tiny"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
   res.json({
     status: "Accepted",
   });
+});
+
+app.get("/index", (req, res) => {
+  res.send("Can't see me!");
 });
 
 app.listen(port, () => {
