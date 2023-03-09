95266b98971248016200de7a7ff016b5e82b93b9
Abhijeet Ingle <abhijeetingle.p@gmail.com>
2023-03-09 12:16:02 +0530

    added pagination to the albumrouter

--- backend/routes/albumRouter.js
+++ backend/routes/albumRouter.js
@@ -29,14 +29,65 @@ router.get("/:id", validateId, async (req, res, next) => {
 router.get("/", async (req, res, next) => {
   const options = {};
 
+  // pagination
+  let { page, results } = req.query;
+  page = parseInt(page);
+  results = parseInt(results);
+  let limit = 5;
+  let offset = 0;
+
   if (Object.keys(req.query).includes("title")) {
     options["where"] = {
       title: req.query["title"],
     };
   }
   try {
-    const albums = await Album.findAll(options);
-    res.json(albums);
+    if (results) {
+      if (results >= 1) {
+        if (page) {
+          if (page > 0) {
+            const count = await Album.count(options);
+            if (page <= Math.ceil(count / results)) {
+              limit = results;
+              offset = (page - 1) * limit;
+
+              options.limit = results;
+              options.offset = offset;
+
+              const albums = await Album.findAll(options);
+              res.json(albums);
+            } else {
+              const err = new Error(
+                `page should be smaller than or equal to ${Math.ceil(
+                  count / results
+                )}`
+              );
+              err.status = 404;
+              next(err);
+            }
+          } else {
+            const err = new Error("page should be greater than or equal to 0");
+            err.status = 404;
+            next(err);
+          }
+        } else {
+          const err = new Error("page should be an integer");
+          err.status = 404;
+          next(err);
+        }
+      } else if (results * page === 0) {
+        const albums = await Album.findAll(options);
+        res.json(albums);
+      } else {
+        const err = new Error("results should be greater than or equal to 1");
+        err.status = 404;
+        next(err);
+      }
+    } else {
+      const err = new Error("results should be an integer");
+      err.status = 404;
+      next(err);
+    }
   } catch (err) {
     next(err);
   }
