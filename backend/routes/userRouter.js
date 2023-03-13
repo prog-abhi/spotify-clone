e38e9cddb61cd6e4f3b525ca8f05d92f00813a9b
Abhijeet Ingle <abhijeetingle.p@gmail.com>
2023-03-13 12:28:05 +0530

    Added options and pagination middlewares to user router.
    Refactored the logic for filtering and pagination from artist router into separate middleware generators in utilities directory.
    Using them added the filetering and pagination support to the user router.

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,39 @@
 const { user: User } = require("../db/models");
+const {
+  validateIdGenerator,
+  optionsGenerator,
+  paginationGenerator,
+} = require("./utilities");
 const router = require("express").Router();
 
-router.get("/", async (req, res, next) => {
-  res.json(await User.findAll());
+const validateId = validateIdGenerator(
+  User,
+  (id) => `user with id ${id} not found!`
+);
+
+const optionsMiddleware = optionsGenerator([
+  "username",
+  "first_name",
+  "last_name",
+]);
+
+const paginationMiddleware = paginationGenerator(User);
+
+router.get("/:id", validateId, async (req, res, next) => {
+  const user = await User.findByPk(req.params.id);
+  res.json(user);
 });
+
+router.get(
+  "/",
+  optionsMiddleware,
+  paginationMiddleware,
+  async (req, res, next) => {
+    const { _options: options } = req;
+    const users = await User.findAll(options);
+    res.json(users);
+  }
+);
 
 module.exports = {
   router,
--- backend/routes/utilities/index.js
+++ backend/routes/utilities/index.js
@@ -19,6 +19,96 @@ function validateIdGenerator(model, errorCb) {
   };
 }
 
+function optionsGenerator(queries) {
+  return (req, res, next) => {
+    const where = {};
+    const queryKeys = Object.keys(req.query);
+
+    queries.forEach((query) => {
+      if (queryKeys.includes(query)) {
+        where[query] = req.query[query];
+      }
+    });
+
+    req._options = { where };
+    next();
+  };
+}
+
+function paginationGenerator(model) {
+  return async (req, res, next) => {
+    let limit = 5;
+    let offset = 0;
+    const queryKeys = Object.keys(req.query);
+    const errorArray = [];
+    const createErrorAndPushToArray = function (msg) {
+      const err = new Error(msg);
+      err.status = 404;
+      errorArray.push(err);
+    };
+
+    if (queryKeys.includes("results")) {
+      if (validator.isInt(req.query.results)) {
+        results = parseInt(req.query.results);
+        if (results >= 1) {
+          if (validator.isInt(req.query.page)) {
+            page = parseInt(req.query.page);
+            if (page > 0) {
+              const count = await model.count(req.options);
+              if (page <= Math.ceil(count / results)) {
+                limit = results;
+                offset = (page - 1) * limit;
+              } else {
+                createErrorAndPushToArray(
+                  `page should be smaller than or equal to ${Math.ceil(
+                    count / results
+                  )}`
+                );
+              }
+            } else {
+              createErrorAndPushToArray(
+                "page should be greater than or equal to 0"
+              );
+            }
+          } else {
+            createErrorAndPushToArray(
+              "page should be present with results with int value"
+            );
+          }
+        } else if (results == 0) {
+          if (validator.isInt(req.query.page)) {
+            if (req.query.page == 0) {
+              limit = offset = null;
+            } else {
+              createErrorAndPushToArray(
+                "when results is 0 page should also be 0"
+              );
+            }
+          } else {
+            createErrorAndPushToArray("page should be an int!");
+          }
+        } else {
+          createErrorAndPushToArray(
+            "result should be greater than or equal to 1."
+          );
+        }
+      } else {
+        createErrorAndPushToArray("result should be an integer!");
+      }
+    }
+
+    if (errorArray.length > 0) {
+      next(errorArray);
+    }
+
+    req._options["limit"] = limit;
+    req._options["offset"] = offset;
+    next();
+  };
+}
+
 module.exports = {
   validateIdGenerator,
+  optionsGenerator,
+  paginationGenerator,
 };
