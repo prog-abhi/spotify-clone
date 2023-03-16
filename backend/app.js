6c2f98c2b823c4888aa28bd8cd553b203ff94fb5
Abhijeet Ingle <abhijeetingle.p@gmail.com>
2023-03-16 20:07:24 +0530

    Refactored the backend with bin folder

--- backend/app.js
+++ backend/app.js
@@ -58,6 +58,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
+module.exports = app;
