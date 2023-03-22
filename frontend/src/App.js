c63dbd68732dc4f9887d34b459c4b5fe84a9abb9
Abhijeet Ingle <abhijeetingle.p@gmail.com>
2023-03-22 15:05:51 +0530

    Created the basic structure of the homepage and added nav bar implementation
    Created the structure of the homapage at route "/homepage" and added jsx for it's
    Navbar component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./components/login";
 import Signup from "./components/signup";
+import Homepage from "./components/homepage";
+import Search from "./components/search";
+import Library from "./components/library";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +17,22 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <Signup />,
+  },
+  {
+    path: "/homepage",
+    element: <Homepage />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+  },
+  {
+    path: "/library",
+    element: <Library />,
+  },
+  {
+    path: "/:userId/playlists/:playlistId",
+    element: <h1>This is the default for now</h1>,
   },
 ]);
 
